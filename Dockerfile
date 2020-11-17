78460b27fb0484266d20f96552f422598fb7acd7
Gautier Bonnemaison <bonnegaut@gmail.com>
2020-11-17 15:42:48 +0100

    added DB support

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM debian
+ARG DBNAME=${DBNAME}
+ARG POSTGRES_USER=${POSTGRES_USER}
+ARG POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
+ARG URL_DB=${URL_DB}
 RUN apt-get update
 RUN apt-get python3 -y
 RUN apt-get install python3-pip -y
 RUN pip3 install flask
+RUN pip3 install psycopg2
 COPY . /app/
 WORKDIR /app
 CMD ["export", "FLASK_APP=app_flask.py"]
--- app_flask.py
+++ app_flask.py
@@ -1,6 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
+#import psycopg2
+
+
+conn = psycopg2.connect(dbname=os.environ['DBNAME'], user=os.environ['POSTGRES_USER'],
+                            password=os.environ['POSTGRES_PASSWORD'], host=os.environ['URL_DB'], port="5432")
+print("Connected to the data base")
+
+cur = conn.cursor()
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html', url="http://surveillancedevieux-flask.apps.asidiras.dev")
+
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    if request.method == 'POST':
+        try:
+            print("Executing SQL query")
+            print(request.form['POST-SQL'])
+            cur.execute(request.form['POST-SQL'])
+        except:
+            return render_template("post_data.html", msg="Error")
+        return render_template("post_data.html", msg="Success")
+    else:
+        try:
+            print("Executing SQL query")
+            print(request.args.get('GET-SQL'))
+            cur.execute(request.form['GET-SQL'])
+        except:
+            return render_template("get_data.html", msg="Error")
+        data = cur.fetchall()
+        return str(data)
